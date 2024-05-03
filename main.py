c820cc62154b62024ae0393bd49fa5831466e24d
Joao Victor <lealjoaovictor2004@gmail.com>
2024-05-02 21:50:02 -0300

    Criação do menu

---
+++ main.py
@@ -0,0 +1,27 @@
+def menu_opt():
+    print("1 - Criar o arquivo de dados\n2 - Incluir um determinado vendedor no arquivo\n3 - Excluir um determinado "
+          "vendedor no arquivo\n4 - Alterar o valor toral da venda de um determinado vendedor de um determinado "
+          "mês\n5 - Imprimir os registros na saída padrão\n6 - Consultar o vendedor com maior valor da venda\n7 - "
+          "Finalizar o programa")
+    usr_opt = input("\nDigite uma opção válida: ")
+
+    if usr_opt == '1':
+        return None
+    elif usr_opt == '2':
+        return None
+    elif usr_opt == '3':
+        return None
+    elif usr_opt == '4':
+        return None
+    elif usr_opt == '5':
+        return None
+    elif usr_opt == '6':
+        return None
+    elif usr_opt == '7':
+        print("Saindo...")
+    else:
+        print("Opção inválida")
+
+
+if __name__ == '__main__':
+    menu_opt()
