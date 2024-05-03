bd1676d0146bc8a4bf9538130d94a713630a2e5c
o-gustavolopes <gustavolopesmartins2506@gmail.com>
2024-05-03 18:08:15 -0300

    Criação das funções "criar_arquivo" e "incluir_vendedor" (experimental).

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def menu_opt():
     if usr_opt == '1':
         criar_arquivo()
     elif usr_opt == '2':
-        return None
+        incluir_vendedor()
     elif usr_opt == '3':
         return None
     elif usr_opt == '4':
@@ -23,7 +23,31 @@ def menu_opt():
         print("Opção inválida")
 
 def criar_arquivo():
-    
+    arquivo_binario = "arquivo_binario.bin"
+
+    try:
+        with open(arquivo_binario, 'wb') as arquivo:
+            pass
+        print(f"\nArquivo binário '{arquivo_binario}' criado com sucesso!\n")
+    except Exception as e:
+        print(f"\nOcorreu um erro ao criar o arquivo binário: {e}\n")
+
+    menu_opt()
+
+def incluir_vendedor():
+    try:
+        codigo_vendedor = int(input("\nDigite o código do vendedor: "))
+        codvend_bytes = codigo_vendedor.to_bytes((codigo_vendedor.bit_length() + 7) // 8, 'big')
+
+        with open('arquivo_binario.bin', 'wb') as arquivo:
+            arquivo.write(codvend_bytes)
+        print("Vendedor incluído com sucesso!")
+    except ValueError:
+        print("O código precisa ser um número inteiro.")
+
+def excluir_vendedor():
+    pass
+
 
 if __name__ == '__main__':
     menu_opt()
