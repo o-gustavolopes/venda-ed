29e40d580f0f5581e0cf3a99c3ba7cd549ec2171
o-gustavolopes <gustavolopesmartins2506@gmail.com>
2024-05-04 11:04:00 -0300

    criacao_funcoes1
    Criação das funções "criar_arquivo", "incluir_vendedor" e "excluir_vendedor".

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def menu_opt():
     elif usr_opt == '2':
         incluir_vendedor()
     elif usr_opt == '3':
-        return None
+        excluir_vendedor()
     elif usr_opt == '4':
         return None
     elif usr_opt == '5':
@@ -21,31 +21,66 @@ def menu_opt():
         print("Saindo...")
     else:
         print("Opção inválida")
+        menu_opt()
 
 def criar_arquivo():
-    arquivo_binario = "arquivo_binario.bin"
+    arquivo = "arquivo.txt"
 
     try:
-        with open(arquivo_binario, 'wb') as arquivo:
+        with open(arquivo, 'w') as arq:
             pass
-        print(f"\nArquivo binário '{arquivo_binario}' criado com sucesso!\n")
+        print(f"\nArquivo '{arquivo}' criado com sucesso!\n")
     except Exception as e:
-        print(f"\nOcorreu um erro ao criar o arquivo binário: {e}\n")
+        print(f"\nOcorreu um erro ao criar o arquivo: {e}\n")
 
     menu_opt()
 
 def incluir_vendedor():
     try:
         codigo_vendedor = int(input("\nDigite o código do vendedor: "))
-        codvend_bytes = codigo_vendedor.to_bytes((codigo_vendedor.bit_length() + 7) // 8, 'big')
 
-        with open('arquivo_binario.bin', 'wb') as arquivo:
-            arquivo.write(codvend_bytes)
-        print("Vendedor incluído com sucesso!")
+        with open('arquivo.txt', 'a') as arquivo:
+            arquivo.write(str(codigo_vendedor) + '\n')
+        print("Vendedor incluído com sucesso!\n")
     except ValueError:
         print("O código precisa ser um número inteiro.")
 
+    menu_opt()
+
 def excluir_vendedor():
+    cod_excluido = input("\nDigite o código do vendedor que deseja excluir (se quiser cancelar a operação digite zero): ")
+    if cod_excluido == 0:
+        print("Operação cancelada.\n")
+        menu_opt()
+    else:
+        with open('arquivo.txt', 'r') as arquivo_leitura:
+            linhas = arquivo_leitura.readlines()
+            vendedor_encontrado = False
+            for linha in linhas:
+                partes = linha.strip().split(',')
+                if partes[0] == cod_excluido:
+                    vendedor_encontrado = True
+
+            if not vendedor_encontrado:
+                print("Código do vendedor não encontrado.")
+                excluir_vendedor()
+            else:
+                with open('arquivo.txt', 'w') as arquivo_escrita:
+                    for linha in linhas:
+                        partes = linha.strip().split(',')
+                        if partes[0] != cod_excluido:
+                            arquivo_escrita.write(linha)
+                print(f"Vendedor {cod_excluido} excluído com sucesso.\n")
+
+    menu_opt()
+
+def alterar_valores():
+    pass
+
+def print_registros():
+    pass
+
+def vendedor_maior_venda():
     pass
 
 
